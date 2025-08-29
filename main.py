fef685e0f0302fe7594c83c9881c8fb46d1bd1f2
Srivatsav Sundaresan <srivatsav.sr2@gmail.com>
2025-08-28 23:36:23 -0700

    Created a pokemon class. open a pack implimented, very basic

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import discord
 import os
 import random
 from dotenv import load_dotenv
+import json
 
 load_dotenv()
 intents = discord.Intents.default()
@@ -9,9 +10,28 @@ intents.message_content = True
 client = discord.Client(intents=intents)
 token = os.getenv('TOKEN')
 
+class Pokemon():
+    def __init__(self, id, name, types, level):
+        self.id = id
+        self.name = name
+        self.type = types
+        self.level = level
+
 @client.event
 async def on_ready():
     print("Logged in as a bot {0.user}".format(client))
+
+# Function to open a pack and return a random Pokemon
+def open_pack():
+    with open('pokemon.json', 'r') as p:
+        data = json.load(p)
+        random_pokemon = random.choice(data)
+        id = random_pokemon.get('id')
+        name = random_pokemon.get('name')
+        types = random_pokemon.get('types')
+        level = random_pokemon.get('level')
+        pokemon = Pokemon(id, name, types, level)
+    return pokemon
 
 @client.event
 async def on_message(message):
@@ -32,8 +52,9 @@ async def on_message(message):
             await message.channel.send(f'Doing backchodi')
         elif user_message.lower() == "bye":
             await message.channel.send(f'Bye {username}')
-        elif user_message.lower() == "is lohit gay":
-            await message.channel.send(f'Yes very gay. In fact so is Kishore')
+        elif user_message.lower() == "open a pack":
+            pokemon = open_pack()
+            await message.channel.send(f'You got!! {pokemon.name}, Types: {pokemon.type}, Levle: {pokemon.level}')
         elif user_message.lower() == "tell me a joke":
             jokes = [" Can someone please shed more\
             light on how my lamp got stolen?",
@@ -42,5 +63,6 @@ async def on_message(message):
                      "What do you call a gazelle in a \
                      lions territory? Denzel."]
             await message.channel.send(random.choice(jokes))
+
 
 client.run(token)
